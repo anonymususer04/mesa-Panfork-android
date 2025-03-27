03c801cebe4ab016de185a9922c0a18807d368b5
Anonymus04 <joaobat0102@gmail.com>
2025-03-27 11:37:02 -0300

    Unknown GPUID alerts

--- src/panfrost/lib/pan_props.c
+++ src/panfrost/lib/pan_props.c
@@ -75,11 +75,12 @@ const struct panfrost_model panfrost_model_list[] = {
         MODEL(0x9093, "G57 r1", "TNAx", HAS_ANISO, 16384, {}),
         MODEL(0x9091, "G57", "TNAx", HAS_ANISO, 16384, {}),
         MODEL(0xa867, "G610", "LODx", HAS_ANISO, 65536, {}),
-        MODEL(0xb8a31030, "G615", "LODx", NO_ANISO, 65536, {}),
         /* Matching the kbase dummy model, probably not real GPUs */
         MODEL(0xa802, "G710", "TODx", HAS_ANISO, 65536, {}),
 };
 
+/* for not supported panfrost gpu driver name (gpu id MODEL) from panfrost_model, 
+your driver gpu name returns [Unknowm Mali Device] */
 
 const struct panfrost_model panfrost_unknown_model = {
    .gpu_id = 0,
